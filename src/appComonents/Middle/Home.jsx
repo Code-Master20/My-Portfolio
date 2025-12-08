638f949cd5d9e77d4032f1ea3e337294c3fcb400
Code-Master20 <sahidurmiah201920@gmail.com>
2025-12-08 18:02:50 +0530

    Home-page designed(for mobile)

--- src/appComonents/Middle/Home.jsx
+++ src/appComonents/Middle/Home.jsx
@@ -1,5 +1,12 @@
 import styles from "./Home.module.css";
 import developerImg from "../../assets/SahidurImage.jpg";
+import classMateAppImg from "../../assets/ClassMate-App.png";
+import mayRigAppImg from "../../assets/mayRig.png";
+import modernTodoAppImg from "../../assets/Modern-Todo-App.png";
+import technoTalkAppImg from "../../assets/Techno-Talk-Pvt-Ltd.png";
+import weatherAppImg from "../../assets/Weather-App.png";
+import weCodeTogetherAppImg from "../../assets/weCodeTogether-App.png";
+
 export const Home = () => {
   return (
     <section className={styles["main-section"]}>
@@ -33,11 +40,33 @@ export const Home = () => {
         </div>
         {/* this current project info */}
         <div className={`${styles["this-info"]}`}>
-          <div>This Prpoject's Info</div>
+          <div>
+            <h1>About This Project :</h1>
+            <p>
+              This is a fully responsive personal portfolio website built using
+              HTML, CSS, TailwindCSS, JavaScript, and ReactJS. The design adapts
+              to all screen sizes, making it device-width and height
+              independent. The site showcases my skills, projects, and
+              experience in a clean and modern layout.
+            </p>
+            <p>
+              I also added a contact form where users can send me a message. For
+              this, I used Node.js, Express, MongoDB, and Mongoose to store the
+              messages, along with a free API integration for additional
+              projects merged with this current project.
+            </p>
+          </div>
         </div>
         {/* My other projects' info */}
         <div className={`${styles["projects-info"]}`}>
-          <div>My Other Projects' Info</div>
+          <div>
+            <img src={classMateAppImg} alt="" />
+            <img src={mayRigAppImg} alt="" />
+            <img src={weatherAppImg} alt="" />
+            <img src={weCodeTogetherAppImg} alt="" />
+            <img src={technoTalkAppImg} alt="" />
+            <img src={modernTodoAppImg} alt="" />
+          </div>
         </div>
       </div>
     </section>
