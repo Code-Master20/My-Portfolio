69b9ea92cf150f08e8322463806ed900fe80ffa6
Code-Master20 <sahidurmiah201920@gmail.com>
2025-12-10 10:58:49 +0530

    About-page done (mobile devices)

--- src/appComonents/Middle/About.jsx
+++ src/appComonents/Middle/About.jsx
@@ -25,15 +25,57 @@ export const About = () => {
         </div>
         {/* developer journeyed learning web development */}
         <div className={`${styles["dev-journey"]}`}>
-          <div>How Developer Journeyed?</div>
+          <div>
+            <h1>My Journey</h1>
+            <p>
+              I began my coding journey after completing my 12th grade in the
+              science stream. Driven by curiosity and passion for technology, I
+              started learning web development on my own, building projects, and
+              exploring modern tools and frameworks.Currently, I am pursuing a
+              degree in <span>BCA</span> , which is expected to be completed by
+              2028.
+            </p>
+            <p>
+              I have experience working with popular technologies like{" "}
+              <span>
+                HTML5, CSS3, TailwindCSS, ReactJS, NodeJS, ExpressJS,
+                JavaScript, Mongodb, Mongoose, Bootstrap
+              </span>
+              , and more. I enjoy tackling challenging problems and building
+              efficient, scalable solutions.
+            </p>
+          </div>
         </div>
         {/* developer acquired skills */}
         <div className={`${styles["dev-skills"]}`}>
-          <div>Developer acquired skills</div>
+          <div>
+            <h1>Skills</h1>
+            <p>
+              <span>HTML5</span>
+              <span>CSS3</span>
+              <span>TailwindCSS</span>
+              <span>Bootstrap</span>
+              <span>JavaScript</span>
+              <span>ReactJs</span>
+              <span>NodeJs</span>
+              <span>ExpressJs</span>
+              <span>Mongodb</span>
+              <span>Mongoose</span>
+              <span>Git</span>
+              <span>etc.</span>
+            </p>
+          </div>
         </div>
         {/* developer acquired experience in the field */}
         <div className={`${styles["dev-experience"]}`}>
-          <div>Developer acquied experience in fields</div>
+          <div>
+            <h1>Experience</h1>
+            <p>
+              Over the years, I've worked on a range of web projects, from small
+              fun-play websites to full-fledged web applications. I'm always
+              looking for new opportunities to grow and make an impact.
+            </p>
+          </div>
         </div>
       </section>
     </section>
