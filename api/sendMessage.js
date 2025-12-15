648580f2eb82ebd39e90fa8f1582239b2dbad8a1
Code-Master20 <sahidurmiah201920@gmail.com>
2025-12-15 23:53:16 +0530

    resend.emails.send is altered

--- api/sendMessage.js
+++ api/sendMessage.js
@@ -25,9 +25,9 @@ export default async function handler(req, res) {
     await resend.emails.send({
       from: "onboarding@resend.dev",
       to: process.env.TO_EMAIL,
-      subject: "New Portfolio Message",
+      subject: "User Query",
       html: `
-        <h3>New Message from Portfolio</h3>
+        <h3>New Message from My-Portfolio</h3>
         <p>${message}</p>
       `,
     });
