2ca13661bc41cfba766a30b29f84752f6f1d282a
Code-Master20 <sahidurmiah201920@gmail.com>
2025-12-16 01:02:19 +0530

    route for Classmate project descriptions

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Home } from "./appComonents/Middle/Home.jsx";
 import { About } from "./appComonents/Middle/About.jsx";
 import { Projects } from "./appComonents/Middle/Projects.jsx";
 import { Contact } from "./appComonents/Middle/Contact.jsx";
+import { Classmate } from "./projectComponents/Classmate.jsx";
 import {
   createBrowserRouter,
   Route,
@@ -17,7 +18,9 @@ const Router = createBrowserRouter(
     <Route path="/" element={<App />}>
       <Route path="" element={<Home />}></Route>
       <Route path="/about" element={<About />} />
-      <Route path="/projects" element={<Projects />} />
+      <Route path="/projects/" element={<Projects />}>
+        <Route path="class-mate" element={<Classmate />} />
+      </Route>
       <Route path="/contact" element={<Contact />} />
     </Route>
   )
