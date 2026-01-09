c90255ef24e44c7ecebef89fa081302c2a35ebd3
Code-Master20 <sahidurmiah201920@gmail.com>
2026-01-10 00:18:10 +0530

    new styls

--- src/appComonents/Middle/Home.jsx
+++ src/appComonents/Middle/Home.jsx
@@ -9,37 +9,31 @@ import weCodeTogetherAppImg from "../../assets/weCodeTogether-App.png";
 
 export const Home = () => {
   return (
-    <section className={styles["main-section"]}>
-      <div>
+    <main className={styles["main-section-one"]}>
+      <section className={styles["main-section-two"]}>
         {/* project owner info */}
-        <div className={`${styles["owner-info"]}`}>
-          <div>
-            {/* developer image */}
-            <div className={`${styles["dev-img-container"]}`}>
-              <div>
-                <img src={developerImg} alt="" />
-                <h1>Mr. Sahidur Miah</h1>
-                <h2>Web Developer(Full-Stack)</h2>
-              </div>
-            </div>
-            {/* developer info */}
-            <div className={`${styles["dev-info-container"]}`}>
-              <div>
-                <h1>hi i'm sahidur miah</h1>
-                <h2>WEB DEVELOPER</h2>
-                <h3>Building modern and impactful web solutions</h3>
-                <h4>About Me :</h4>
-                <p>
-                  I am a web developer with a passion for creating sleek and
-                  modern web applications. I love transforming ideas into
-                  reality using the latest tech.
-                </p>
-              </div>
-            </div>
+        <article className={`${styles["owner-info"]}`}>
+          {/* developer image */}
+          <div className={`${styles["dev-img-container"]}`}>
+            <img src={developerImg} alt="" />
+            <p>Mr. Sahidur Miah</p>
+            <p>Web Developer(Full-Stack)</p>
           </div>
-        </div>
+          {/* developer info */}
+          <div className={`${styles["dev-info-container"]}`}>
+            <h1>hi i'm sahidur miah</h1>
+            <h2>WEB DEVELOPER</h2>
+            <h3>Building modern and impactful web solutions</h3>
+            <h4>About Me :</h4>
+            <p>
+              I am a web developer with a passion for creating sleek and modern
+              web applications. I love transforming ideas into reality using the
+              latest tech.
+            </p>
+          </div>
+        </article>
         {/* this current project info */}
-        <div className={`${styles["this-info"]}`}>
+        {/* <article className={`${styles["this-info"]}`}>
           <div>
             <div>
               <h1>About This Project :</h1>
@@ -58,9 +52,9 @@ export const Home = () => {
               </p>
             </div>
           </div>
-        </div>
+        </article> */}
         {/* My other projects' info */}
-        <div className={`${styles["projects-info"]}`}>
+        {/* <article className={`${styles["projects-info"]}`}>
           <div>
             <img src={classMateAppImg} alt="" />
             <img src={mayRigAppImg} alt="" />
@@ -69,8 +63,8 @@ export const Home = () => {
             <img src={technoTalkAppImg} alt="" />
             <img src={modernTodoAppImg} alt="" />
           </div>
-        </div>
-      </div>
-    </section>
+        </article> */}
+      </section>
+    </main>
   );
 };
