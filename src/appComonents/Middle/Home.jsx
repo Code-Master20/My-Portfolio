cbab2bd564e68ada2064965a2c9f7b265b7e7536
Code-Master20 <sahidurmiah201920@gmail.com>
2026-01-13 22:52:27 +0530

    current project details added

--- src/appComonents/Middle/Home.jsx
+++ src/appComonents/Middle/Home.jsx
@@ -33,26 +33,24 @@ export const Home = () => {
           </div>
         </article>
         {/* this current project info */}
-        {/* <article className={`${styles["this-info"]}`}>
+        <article className={`${styles["this-info"]}`}>
           <div>
-            <div>
-              <h1>About This Project :</h1>
-              <p>
-                This is a fully responsive personal portfolio website built
-                using HTML, CSS, TailwindCSS, JavaScript, and ReactJS. The
-                design adapts to all screen sizes, making it device-width and
-                height independent. The site showcases my skills, projects, and
-                experience in a clean and modern layout.
-              </p>
-              <p>
-                I also added a contact form where users can send me a message.
-                For this, I used Node.js, Express, MongoDB, and Mongoose to
-                store the messages, along with a free API integration for
-                additional projects merged with this current project.
-              </p>
-            </div>
+            <h1>About This Project :</h1>
+            <p>
+              This is a fully responsive personal portfolio website built using
+              HTML, CSS, TailwindCSS, JavaScript, and ReactJS. The design adapts
+              to all screen sizes, making it device-width and height
+              independent. The site showcases my skills, projects, and
+              experience in a clean and modern layout.
+            </p>
+            <p>
+              I also added a contact form where users can send me a message. For
+              this, I used Node.js, Express, MongoDB, and Mongoose to store the
+              messages, along with a free API integration for additional
+              projects merged with this current project.
+            </p>
           </div>
-        </article> */}
+        </article>
         {/* My other projects' info */}
         {/* <article className={`${styles["projects-info"]}`}>
           <div>
